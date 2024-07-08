53837e4e6ea32373c1e6f80598ac2b5d7c2b24f7
jcanaviri42 <jcanaviri42@gmail.com>
2024-07-08 15:43:38 -0400

    feat: exploring the basic things of angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PersonsComponent } from './persons/persons.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PersonsComponent],
+  imports: [RouterOutlet, PersonsComponent, CalculatorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
