41dbadd4eaa0de4e69ffda3d59e15af7d0c1cc23
jcanaviri42 <jcanaviri42@gmail.com>
2024-07-09 23:13:11 -0400

    feat: using *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,31 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PersonsComponent } from './persons/persons.component';
 import { CalculatorComponent } from './calculator/calculator.component';
+import { FormsModule } from '@angular/forms';
+import { Person } from './persons/person.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PersonsComponent, CalculatorComponent],
+  imports: [RouterOutlet, PersonsComponent, CalculatorComponent, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Angular';
-  greeting = 'Greetings'
+  title = 'Listing Persons';
+  inputFirstName = '';
+  inputLastName = '';
+
+  people: Person[] = [
+    new Person('John', 'Smith'),
+    new Person('Susan', 'Anniston'),
+  ];
+
+  addPerson() {
+    if (this.inputFirstName === '' && this.inputLastName === '') return;
+    const newPerson = new Person(this.inputFirstName, this.inputLastName);
+
+    this.people.push(newPerson);
+  }
 }
---
+++ src/app/persons/person.model.ts
@@ -0,0 +1,11 @@
+export class Person {
+  // name: string;
+  // lastName: string;
+
+  // constructor(name: string, lastName: string) {
+  //   this.name = name;
+  //   this.lastName = lastName;
+  // }
+
+  constructor(public firstName: string, public lastName: string) {}
+}
