cbe1ec70eddd68721ea7cb493fc44f1f1580fd9b
jcanaviri42 <jcanaviri42@gmail.com>
2024-07-16 11:11:47 -0400

    fix: clean up the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,11 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-import { CalculatorComponent } from './calculator/calculator.component';
-import { FormComponent } from "./form/form.component";
-import { PersonComponent } from './person/person.component';
-import { Person } from './persons/person.model';
-import { PersonsComponent } from './persons/persons.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    PersonsComponent,
-    CalculatorComponent,
-    FormsModule,
-    CommonModule,
-    PersonComponent,
-    FormComponent
-],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'Listing Persons';
-  inputFirstName = '';
-  inputLastName = '';
-
-  people: Person[] = [];
-
-  addPerson(person: Person) {
-    this.people.push(person);
-  }
-}
+export class AppComponent {}
